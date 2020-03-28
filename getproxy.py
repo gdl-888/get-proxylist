2bfd6da39389b23668e27f1af2905736f0372289
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-03-28 07:39:24 -0700

    -

--- getproxy.py
+++ getproxy.py
@@ -1,12 +1,17 @@
 import re
 import requests
 
-rawList = requests.get("https://www.proxy-list.download/api/v1/get?type=http").text;
+reqTypes = ['http', 'https', 'socks4', 'socks5'];
+
+rawList = "";
+
+for reqType in reqTypes:
+    rawList += requests.get("https://www.proxy-list.download/api/v1/get?type=" + reqType).text;
+    print(reqType.upper() + " proxies added.");
+    rawList += "\r\n";
 
 rawList = re.sub("\r", "", rawList);
 ProxyList = re.sub("[:]\d{1,6}$", "", rawList, flags=re.MULTILINE);
-
-print(ProxyList);
 
 f = open("prxylist.txt", "w+");
 f.write("Total " + str(len(ProxyList.split("\n"))) + " Proxies \r\n \r\n" + ProxyList);
