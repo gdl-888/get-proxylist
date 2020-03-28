81fd3dbcbbd5e31b81d6a0740c72b2b071fb0ae5
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-03-28 07:36:22 -0700

    -

--- getproxy.py
+++ getproxy.py
@@ -9,5 +9,5 @@ ProxyList = re.sub("[:]\d{1,6}$", "", rawList, flags=re.MULTILINE);
 print(ProxyList);
 
 f = open("prxylist.txt", "w+");
-f.write(ProxyList);
+f.write("Total " + str(len(ProxyList.split("\n"))) + " Proxies \r\n \r\n" + ProxyList);
 f.close();
