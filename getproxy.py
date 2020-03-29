47021b0507f91c447eab3632a5cbf455442f9149
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-03-28 18:53:37 -0700

    -

--- getproxy.py
+++ getproxy.py
@@ -13,6 +13,15 @@ for reqType in reqTypes:
 rawList = re.sub("\r", "", rawList);
 ProxyList = re.sub("[:]\d{1,6}$", "", rawList, flags=re.MULTILINE);
 
+RevList = "";
+
+for ip in ProxyList.split('\n'):
+    RevList = ip + "\n" + RevList;
+
 f = open("prxylist.txt", "w+");
 f.write("Total " + str(len(ProxyList.split("\n"))) + " Proxies \r\n \r\n" + ProxyList);
+f.close();
+
+f = open("rpxylist.txt", "w+");
+f.write("Total " + str(len(RevList.split("\n"))) + " Proxies in reversed order \r\n \r\n" + RevList);
 f.close();
